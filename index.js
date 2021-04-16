78e8cb99e58038ad7be2e432c0f95e2716251b9e
ericmuh <muhwezi1000@gmail.com>
2021-04-16 17:13:47 +0300

    updates

---
+++ index.js
@@ -0,0 +1,30 @@
+// we use let or const instead of var for declaration of variables
+console.log("Somthing is happening");
+// Types : INBUILT TYPES BY TS.
+//type string.
+var myName = "Eric";
+//type boolean.
+var truthy; // declaring a variable and not assigned it
+// type number...
+var num;
+// number array...
+var myNumArray;
+myNumArray = [1, 3, 6];
+// string array..
+var myStringArray = ["hello", "hhh"];
+//boolean array ..
+var myBooleanArray = [true, false];
+// object array ..
+var myObjArray = [{ eric: "eric" }, [1, 2, 2]];
+// type object
+var myObj = {
+    name: "Eric",
+    age: 30
+};
+// object literals ...  key pair values ...
+var myDic = {
+    id: 2,
+    book_name: "KingMans"
+};
+// type unions
+var myId = "122334";
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 
 // we use let or const instead of var for declaration of variables
 
+console.log("Somthing is happening")
 // Types : INBUILT TYPES BY TS.
 
 //type string.
@@ -31,3 +32,54 @@ let myObj:object ={
     age:30,
 }
 
+// object literals ...  key pair values ...
+
+
+let myDic:{id:number; book_name:string; isbn:number} = {
+    id:2,
+    book_name:"KingMans",
+    isbn: 9088
+}
+
+
+// type unions
+let myId:string | number |boolean = "122334";
+myId = 144555;
+
+// type alias
+type StringNumber = string | number |boolean;
+
+let myUniqueId :StringNumber = true;
+myUniqueId = 12345;
+
+// type alias for object literals
+type UserType  = {
+    id:number;
+    name:string;
+    password:string
+}
+
+const User1:UserType = {
+    id:1,
+    name:"John",
+    password:"Password123"
+}
+
+// anohter way of making type alias for object literals INTERFACE .
+// Interface ....
+
+interface IBook  {
+name:string;
+id:number|string;
+isbn : number;
+author: string;
+date_recovered ?:string;
+
+}
+
+const BookOne :IBook = {
+name:"Romeo",
+id:5050,
+isbn: 8386776,
+author:"John Doe"
+}
